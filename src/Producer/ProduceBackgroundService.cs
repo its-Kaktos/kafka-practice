dfd537aa5e8169a2f47288259279e69807bd9934
its-kaktos <arman.hmpr@gmail.com>
2025-02-16 23:42:50 +0330

    Re-use producer between messages and enable error and statics handler

--- src/Producer/ProduceBackgroundService.cs
+++ src/Producer/ProduceBackgroundService.cs
@@ -1,34 +1,41 @@
 using System.Globalization;
+using System.Text.Json;
 using Confluent.Kafka;
 
 namespace Producer;
 
-public class ProduceBackgroundService : BackgroundService
+public sealed class ProduceBackgroundService : BackgroundService
 {
-    private readonly ProducerConfig _config = new()
-    {
-        Acks = Acks.All,
-        BootstrapServers = "localhost:9092",
-        RetryBackoffMs = 100,
-        RetryBackoffMaxMs = 1_000,
-        MessageSendMaxRetries = 10,
-        EnableIdempotence = true
-    };
-
+    private readonly IProducer<Null, string> _producer;
     private readonly ILogger<ProduceBackgroundService> _logger;
 
     public ProduceBackgroundService(ILogger<ProduceBackgroundService> logger)
     {
         _logger = logger;
+
+        var config = new ProducerConfig()
+        {
+            Acks = Acks.All,
+            BootstrapServers = "localhost:9092",
+            RetryBackoffMs = 100,
+            RetryBackoffMaxMs = 1_000,
+            MessageSendMaxRetries = 10,
+            EnableIdempotence = true,
+            StatisticsIntervalMs = 10_000,
+            Debug = "broker,topic,msg"
+        };
+
+        _producer = new ProducerBuilder<Null, string>(config)
+            .SetErrorHandler((_, error) => _logger.LogError("Kafka producer exception occured, exception is {Exception}", JsonSerializer.Serialize(error)))
+            .SetStatisticsHandler((_, s) => _logger.LogInformation(s))
+            .Build();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var p = new ProducerBuilder<Null, string>(_config).Build();
-
-            var x = await p.ProduceAsync("practice.kafka.background_service", new Message<Null, string>()
+            var x = await _producer.ProduceAsync("practice.kafka.background_service", new Message<Null, string>()
             {
                 // Key = new Null(),
                 Headers = null,
@@ -36,9 +43,17 @@ public class ProduceBackgroundService : BackgroundService
                 Value = "hello this message was created at " + DateTime.Now
             }, stoppingToken);
 
-            _logger.LogInformation("produced single event to Kafka, time {Time}", DateTime.Now.ToString("O",CultureInfo.CurrentCulture));
+            _logger.LogInformation("produced single event to Kafka, time {Time}", DateTime.Now.ToString("O", CultureInfo.CurrentCulture));
 
-            // await Task.Delay(5_000, stoppingToken);
+            await Task.Delay(1_000, stoppingToken);
         }
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+
+        _producer.Flush();
+        _producer.Dispose();
     }
 }
